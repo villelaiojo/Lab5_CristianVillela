8f7a516e95659199a746805d85b5ef60c1f4be29
villelaiojo <alguien9867@gmail.com>
2023-04-30 11:15:21 -0600

    libreria jason

---
+++ Lab2_Cristian_Villela/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Lab2_Cristian_Villela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
