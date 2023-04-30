56457e447ddcf58454624fd44cbcdae29aa4ec91
villelaiojo <alguien9867@gmail.com>
2023-04-30 11:28:55 -0600

    Clases

--- Lab2_Cristian_Villela/Program.cs
+++ Lab2_Cristian_Villela/Program.cs
@@ -11,6 +11,57 @@ namespace Lab2_Cristian_Villela
 {
     class Program
     {
+        public class House
+        {
+            public string zoneDangerous { get; set; }
+            public string address { get; set; }
+            public double price { get; set; }
+            public string contactPhone { get; set; }
+            public string id { get; set; }
+        }
+        public class Apartment
+        {
+            public bool isPetFriendly { get; set; }
+            public string address { get; set; }
+            public double price { get; set; }
+            public string contactPhone { get; set; }
+            public string id { get; set; }
+        }
+        public class Premise
+        {
+            public string[] commercialActivities { get; set; }
+            public string address { get; set; }
+            public double price { get; set; }
+            public string contactPhone { get; set; }
+            public string id { get; set; }
+        }
+        public class Builds
+        {
+            public Premise[]? Premises { get; set; }
+            public Apartment[]? Apartments { get; set; }
+            public House[]? Houses { get; set; }
+        }
+        public class Input1
+        {
+            public Dictionary<string, bool> services { get; set; }
+            public Builds builds { get; set; }
+
+        }
+        public class Input2
+        {
+            public double budget { get; set; }
+            public string typeBuilder { get; set; }
+            public string[] requiredServices { get; set; }
+            public string commercialActivity { get; set; }
+            public bool? wannaPetFriendly { get; set; }
+            public string minDanger { get; set; }
+        }
+        public class InputLab
+        {
+            public Input1[] input1 { get; set; }
+            public Input2 input2 { get; set; }
+        }
+
         static void Main(string[] args)
         {
             foreach (string line in System.IO.File.ReadLines(@"C:\Users\Cristia Villela\OneDrive\Escritorio\Lab5_CristianVillela\Lab2_Cristian_Villela\bin\Debug\netcoreapp3.1\input_challenge_lab_2.jsonl"))
@@ -219,7 +270,7 @@ namespace Lab2_Cristian_Villela
 
 
             }
-
+            Console.ReadKey();
         }
     }
 }
