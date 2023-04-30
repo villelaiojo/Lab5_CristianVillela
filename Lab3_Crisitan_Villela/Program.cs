192b36e50e36fc9093b67ba8154b052ea7130fcc
villelaiojo <alguien9867@gmail.com>
2023-04-30 11:47:50 -0600

    librerias

---
+++ Lab3_Crisitan_Villela/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Runtime.CompilerServices;
+using static Lab3_Crisitan_Villela.Program;
+using System.Collections.Generic;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+using System.Linq;
+
+namespace Lab3_Crisitan_Villela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
